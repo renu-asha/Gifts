2bc367851b159c381bd321a6c7ea9baa44c0342a
renu-asha <learning960108@gmail.com>
2025-01-12 06:25:25 +0530

    cm1

---
+++ src/main/java/com/example/gifts/GiftsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gifts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gifts/GiftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gifts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
